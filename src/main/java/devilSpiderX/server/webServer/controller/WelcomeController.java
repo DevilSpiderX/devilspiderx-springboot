e0cc1362cc6f8433d62d8f0e890bac911c66e605
DevilSpiderX <62919058+DevilSpiderX@users.noreply.github.com>
2022-10-08 14:36:05 +0800

    2022.10.08 14:35

--- src/main/java/devilSpiderX/server/webServer/controller/WelcomeController.java
+++ src/main/java/devilSpiderX/server/webServer/controller/WelcomeController.java
@@ -3,17 +3,10 @@ package devilSpiderX.server.webServer.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpSession;
-
 @Controller
 public class WelcomeController {
     @GetMapping("/")
-    public String welcome(HttpSession session) {
-//        if (UserFilter.isOperable(session)) {
-//            return "redirect:/index.html";
-//        } else {
-//            return "redirect:/login.html";
-//        }
-        return "/welcome.html";
+    public String welcome() {
+        return "/index.html";
     }
 }
