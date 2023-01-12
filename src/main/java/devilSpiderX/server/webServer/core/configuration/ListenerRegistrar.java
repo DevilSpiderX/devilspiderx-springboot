b664766c9d90c74dc91a5e651f029184adb92652
DevilSpiderX <775716441@qq.com>
2023-01-12 20:20:34 +0800

    1

--- src/main/java/devilSpiderX/server/webServer/core/configuration/ListenerRegistrar.java
+++ src/main/java/devilSpiderX/server/webServer/core/configuration/ListenerRegistrar.java
@@ -1,7 +1,6 @@
 package devilSpiderX.server.webServer.core.configuration;
 
 import devilSpiderX.server.webServer.core.listener.HttpSessionRegister;
-import devilSpiderX.server.webServer.core.listener.MyRequestListener;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class ListenerRegistrar implements WebMvcConfigurer {
     @Bean
     public ServletListenerRegistrationBean<HttpSessionRegister> regHttpSessionRegister(HttpSessionRegister listener) {
-        return new ServletListenerRegistrationBean<>(listener);
-    }
-
-    @Bean
-    public ServletListenerRegistrationBean<MyRequestListener> regMyRequestListener(MyRequestListener listener) {
         return new ServletListenerRegistrationBean<>(listener);
     }
 }
