b2e19e77f694d0b2cd8f67f44978613b90ef32b2
DevilSpiderX <775716441@qq.com>
2023-02-17 01:55:55 +0800

    1.404时直接返回index.html，适配前端WebHistory模式

--- src/main/java/devilSpiderX/server/webServer/core/controller/ErrorController.java
+++ src/main/java/devilSpiderX/server/webServer/core/controller/ErrorController.java
@@ -6,12 +6,18 @@ import devilSpiderX.server.webServer.core.util.AjaxResp;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
 
 @Controller
 @RequestMapping("/error")
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+
+    @GetMapping("/404")
+    public ModelAndView error404() {
+        return new ModelAndView("forward:/", HttpStatus.OK);
+    }
 
     @GetMapping("/{status:\\d+}")
     public String error_get(@PathVariable String status) {
