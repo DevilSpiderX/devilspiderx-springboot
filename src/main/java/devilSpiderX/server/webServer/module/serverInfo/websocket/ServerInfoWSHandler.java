e6d8bacbacd3e4e38a7d006e7ed7a8412ce87488
DevilSpiderX <775716441@qq.com>
2023-04-30 04:00:34 +0800

    1.WebSocket中推送消息改为使用Timer和TimerTask

--- src/main/java/devilSpiderX/server/webServer/module/serverInfo/websocket/ServerInfoWSHandler.java
+++ src/main/java/devilSpiderX/server/webServer/module/serverInfo/websocket/ServerInfoWSHandler.java
@@ -17,9 +17,8 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.lang.ref.WeakReference;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
@@ -29,6 +28,8 @@ public class ServerInfoWSHandler extends TextWebSocketHandler {
     private final ServerInfoService serverInfoService;
     private final TokenService tokenService;
     private final Map<String, Attribute> attributeMap = new HashMap<>();
+    private final Timer senderTimer = new Timer();
+    private final Map<String, TimerTask> sendTaskMap = new HashMap<>();
 
     public ServerInfoWSHandler(ServerInfoService serverInfoService, TokenService tokenService) {
         this.serverInfoService = serverInfoService;
@@ -38,107 +39,129 @@ public class ServerInfoWSHandler extends TextWebSocketHandler {
     record Attribute(String uid, User user, String address, String token) {
     }
 
-    private final Map<String, Thread> timingSendInfoThreadMap = new HashMap<>();
-
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
-        Map<String, Object> map = session.getAttributes();
-        String uid = (String) map.get("uid");
-        User user = (User) map.get("user");
-        String address = (String) map.get("address");
-        String token = (String) map.get("token");
-        attributeMap.put(session.getId(), new Attribute(uid, user, address, token));
-        info(address, "客户端" + uid + "接入");
-        info(address, "当前在线数量为：" + onlineCount.incrementAndGet());
+        final String sessionId = session.getId();
+        final Map<String, Object> map = session.getAttributes();
+        final String uid = (String) map.get("uid");
+        final User user = (User) map.get("user");
+        final String address = (String) map.get("address");
+        final String token = (String) map.get("token");
+        attributeMap.put(sessionId, new Attribute(uid, user, address, token));
+        info(address, "用户%s接入，客户端id为%s".formatted(uid, sessionId));
+        info(address, "当前在线数量为：%d".formatted(onlineCount.incrementAndGet()));
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
-        String sessionId = session.getId();
-        Thread timingSendInfoThread = timingSendInfoThreadMap.remove(sessionId);
-        if (timingSendInfoThread != null && !timingSendInfoThread.isInterrupted()) {
-            timingSendInfoThread.interrupt();
-        }
-        Attribute attr = attributeMap.remove(sessionId);
+        final String sessionId = session.getId();
+        final Attribute attr = attributeMap.remove(sessionId);
         tokenService.destroy(attr.uid(), attr.token());
-        info(attr.address(), "客户端" + attr.uid() + "退出 - "
-                             + CloseReason.CloseCodes.getCloseCode(status.getCode()));
-        info(attr.address(), "当前在线数量为：" + onlineCount.decrementAndGet());
+        info(attr.address(), "客户端%s退出 - %s".formatted(
+                sessionId,
+                CloseReason.CloseCodes.getCloseCode(status.getCode())
+        ));
+        info(attr.address(), "当前在线数量为：%d".formatted(onlineCount.decrementAndGet()));
+        final TimerTask task = sendTaskMap.remove(sessionId);
+        if (task != null) {
+            task.cancel();
+        }
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) {
-        String msg = message.getPayload();
-        String sessionId = session.getId();
-        Attribute attr = attributeMap.get(sessionId);
-        info(attr.address(), "来自客户端" + attr.uid() + "的消息 - " + msg);
-        JSONObject data = JSON.parseObject(msg);
+        final String msg = message.getPayload();
+        final String sessionId = session.getId();
+        final Attribute attr = attributeMap.get(sessionId);
+        info(attr.address(), "来自客户端%s的消息 - %s".formatted(sessionId, msg));
+        final JSONObject data = JSON.parseObject(msg);
         if ("start".equals(data.getString("cmd"))) {
-            Thread timingSendInfoThread = new Thread(
-                    () -> sendServerInfo(session, data.getLong("cd")),
-                    "timing_send_info_" + sessionId
-            );
-            Thread lastThread = timingSendInfoThreadMap.put(sessionId, timingSendInfoThread);
-            if (lastThread != null) {
-                lastThread.interrupt();
-                logger.info("中止上个定时线程");
+            logger.info("客户端{}开始定时任务", sessionId);
+            final TimerTask task = new SendTask(session);
+            final TimerTask lastTask = sendTaskMap.put(sessionId, task);
+            if (lastTask != null) {
+                logger.info("客户端{}中止上个定时任务", sessionId);
+                lastTask.cancel();
             }
-            timingSendInfoThread.start();
+            senderTimer.scheduleAtFixedRate(task, 0, data.getLongValue("cd", 1500));
+        } else if ("stop".equals(data.getString("cmd"))) {
+            logger.info("客户端{}停止定时任务", sessionId);
+            final TimerTask task = sendTaskMap.remove(sessionId);
+            if (task != null) {
+                task.cancel();
+            }
         }
     }
 
-    public void sendServerInfo(WebSocketSession session, long cd) {
-        logger.info("定时线程任务开始");
-        Attribute attr = attributeMap.get(session.getId());
-        try {
-            while (!Thread.currentThread().isInterrupted()) {
-                JSONObject data = new JSONObject();
+    public JSONObject getServerInfo() {
+        final JSONObject data = new JSONObject();
 
-                CPU cpu = serverInfoService.getCPU();
-                data.put("cpu", serverInfoService.constructCpuObject(cpu));
+        final CPU cpu = serverInfoService.getCPU();
+        data.put("cpu", serverInfoService.constructCpuObject(cpu));
 
-                Memory memory = serverInfoService.getMemory();
-                data.put("memory", serverInfoService.constructMemoryObject(memory));
+        final Memory memory = serverInfoService.getMemory();
+        data.put("memory", serverInfoService.constructMemoryObject(memory));
 
-                JSONArray diskDataArray = new JSONArray();
-                List<Disk> disks = serverInfoService.getDisks();
-                for (Disk disk : disks) {
-                    diskDataArray.add(serverInfoService.constructDiskObject(disk));
-                }
-                data.put("disk", diskDataArray);
-
-                Network AllNet = new Network("All", 0, 0, 0);
-                for (Network network : serverInfoService.getNetworks()) {
-                    AllNet.setUploadSpeed(AllNet.getUploadSpeed() + network.getUploadSpeed());
-                    AllNet.setDownloadSpeed(AllNet.getDownloadSpeed() + network.getDownloadSpeed());
-                }
-                data.put("network", serverInfoService.constructNetworkObject(AllNet));
-
-                CurrentOS currentOS = serverInfoService.getCurrentOS();
-                data.put("os", serverInfoService.constructCurrentOSObject(currentOS));
-
-                try {
-                    session.sendMessage(new TextMessage(data.toString()));
-                } catch (IOException e) {
-                    logger.error("定时线程发送信息报错{}", e.getClass().getName());
-                    logger.error(e.getMessage(), e);
-                }
-
-                if (!tokenService.check(attr.uid(), attr.token())) {
-                    session.close(CloseStatus.NORMAL.withReason("token已超时"));
-                }
-                //noinspection BusyWait
-                Thread.sleep(cd);
-            }
-        } catch (InterruptedException ignore) {
-        } catch (IOException e) {
-            logger.error(e.getMessage(), e);
-            logger.error("关闭WebSocket失败,uid: {} ,sessionId: {}", attr.uid(), session.getId());
+        final JSONArray diskDataArray = new JSONArray();
+        final List<Disk> disks = serverInfoService.getDisks();
+        for (Disk disk : disks) {
+            diskDataArray.add(serverInfoService.constructDiskObject(disk));
         }
-        logger.info("定时线程任务结束");
+        data.put("disk", diskDataArray);
+
+        final Network AllNet = new Network("All", 0, 0, 0);
+        for (Network network : serverInfoService.getNetworks()) {
+            AllNet.setUploadSpeed(AllNet.getUploadSpeed() + network.getUploadSpeed());
+            AllNet.setDownloadSpeed(AllNet.getDownloadSpeed() + network.getDownloadSpeed());
+        }
+        data.put("network", serverInfoService.constructNetworkObject(AllNet));
+
+        final CurrentOS currentOS = serverInfoService.getCurrentOS();
+        data.put("os", serverInfoService.constructCurrentOSObject(currentOS));
+
+        return data;
     }
 
     private void info(String address, String msg) {
         logger.info("（{}） {}", address, msg);
+    }
+
+    private class SendTask extends TimerTask {
+        private final WeakReference<WebSocketSession> sessionRef;
+        private int index = 0;
+
+        public SendTask(WebSocketSession session) {
+            this.sessionRef = new WeakReference<>(session);
+        }
+
+        @Override
+        public void run() {
+            final WebSocketSession session = sessionRef.get();
+            if (session == null) {
+                return;
+            }
+            final String sessionId = session.getId();
+            final Attribute attr = attributeMap.get(sessionId);
+            final JSONObject data = getServerInfo();
+            data.put("index", index++);
+
+            try {
+                session.sendMessage(new TextMessage(data.toString()));
+            } catch (IOException e) {
+                logger.error("发送信息报错{}", e.getClass().getName());
+                logger.error(e.getMessage(), e);
+            }
+
+            if (!tokenService.check(attr.uid(), attr.token())) {
+                try {
+                    session.close(CloseStatus.NORMAL.withReason("token已超时"));
+                } catch (IOException e) {
+                    logger.error(e.getMessage(), e);
+                    logger.error("关闭WebSocket失败,uid: {} ,sessionId: {}", attr.uid(), sessionId);
+                } finally {
+                    sendTaskMap.get(sessionId).cancel();
+                }
+            }
+        }
     }
 }
