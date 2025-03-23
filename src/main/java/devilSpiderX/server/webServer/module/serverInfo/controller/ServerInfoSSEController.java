dd38c783b5ce7ece9330cc0d1a8735022a8f7ef1
DevilSpiderX <775716441@qq.com>
2025-03-23 22:10:50 +0800

    1.使用request.startAsync()开启异步上下文，防止发送sse报文阻塞请求处理线程

--- src/main/java/devilSpiderX/server/webServer/module/serverInfo/controller/ServerInfoSSEController.java
+++ src/main/java/devilSpiderX/server/webServer/module/serverInfo/controller/ServerInfoSSEController.java
@@ -3,7 +3,11 @@ package devilSpiderX.server.webServer.module.serverInfo.controller;
 import cn.dev33.satoken.stp.StpUtil;
 import devilSpiderX.server.webServer.core.util.JacksonUtil;
 import devilSpiderX.server.webServer.module.serverInfo.service.ServerInfoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Nonnull;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,13 +18,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.Objects;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Tag(name = "系统软硬件信息SSE接口")
 @RestController
 @RequestMapping("/api/ServerInfo")
 public class ServerInfoSSEController {
     private static final Logger logger = LoggerFactory.getLogger(ServerInfoSSEController.class);
-    public static final String notLoginEvent = "event:notLogin\ndata:[DONE]\n\n";
+    private static final String notLoginEvent = "event:notLogin\ndata:[DONE]\n\n";
+    private static final ThreadFactory threadFactory = new ThreadFactory() {
+        private final AtomicLong counter = new AtomicLong();
+
+        @Override
+        public Thread newThread(@Nonnull final Runnable runnable) {
+            final var num = counter.getAndIncrement();
+            return Thread.ofVirtual()
+                    .name("sse-emitter-%d".formatted(num))
+                    .unstarted(runnable);
+        }
+    };
 
     private final ServerInfoService serverInfoService;
 
@@ -28,38 +46,54 @@ public class ServerInfoSSEController {
         this.serverInfoService = serverInfoService;
     }
 
+    @Operation(summary = "系统信息SSE")
     @GetMapping("sse")
     public void sendServerInfo(
+            @Parameter(description = "推送时间间隔")
             @RequestParam(defaultValue = "1000") final long cd,
+            final HttpServletRequest request,
             final HttpServletResponse response
     ) {
+        final var asyncContext = request.startAsync();
+        asyncContext.setTimeout(0);
+
         response.setStatus(HttpServletResponse.SC_OK);
         response.setContentType(MediaType.TEXT_EVENT_STREAM_VALUE);
         response.setCharacterEncoding("UTF-8");
         response.setHeader("Cache-Control", "no-cache");
         response.setHeader("Connection", "keep-alive");
 
-        try (final var writer = response.getWriter()) {
-            final var currentThread = Thread.currentThread();
-            while (!currentThread.isInterrupted()) {
-                if (!StpUtil.isLogin()) {
-                    writer.print(notLoginEvent);
-                    writer.flush();
-                    break;
-                }
+        final var token = StpUtil.getTokenValue();
 
-                final var data = serverInfoService.getServerInfo();
-                final var event = "event:message\ndata:%s\n\n".formatted(
-                        JacksonUtil.toJSONString(data)
-                                .replaceAll("\n", "\ndata:")
-                );
-                writer.print(event);
-                writer.flush();
-                Thread.sleep(cd);
-            }
-        } catch (IOException | InterruptedException ignored) {
-        } catch (RuntimeException e) {
-            logger.error(e.getMessage(), e);
-        }
+        threadFactory.newThread(() -> {
+                    final var currentThread = Thread.currentThread();
+
+                    try (final var writer = response.getWriter()) {
+                        while (!currentThread.isInterrupted()) {
+                            if (Objects.isNull(StpUtil.getLoginIdByToken(token))) {
+                                writer.print(notLoginEvent);
+                                writer.flush();
+                                break;
+                            }
+
+                            final var data = serverInfoService.getServerInfo();
+                            final var event = "event:message\ndata:%s\n\n".formatted(
+                                    JacksonUtil.toJSONString(data)
+                                            .replaceAll("\n", "\ndata:")
+                            );
+                            writer.print(event);
+                            writer.flush();
+                            Thread.sleep(cd);
+                        }
+                    } catch (IOException ignored) {
+                    } catch (InterruptedException e) {
+                        currentThread.interrupt();
+                    } catch (RuntimeException e) {
+                        logger.error(e.getMessage(), e);
+                    } finally {
+                        asyncContext.complete();
+                    }
+                })
+                .start();
     }
 }
